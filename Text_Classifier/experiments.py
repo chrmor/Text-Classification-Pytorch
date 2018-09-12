92831e16b06c6428c46a2806f27e176e65bbd18e
Christian Morbidoni <christian.morbidoni@gmail.com>
2018-09-12 11:07:08 +0200

    exps

--- Text_Classifier/experiments.py
+++ Text_Classifier/experiments.py
@@ -26,7 +26,7 @@ experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_
 
 experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.01 --max_length 2000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 10 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 2000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 10 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True -- patience 5 --th 0.0")
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 2000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 10 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 
 
 for exp in experiments_to_run:

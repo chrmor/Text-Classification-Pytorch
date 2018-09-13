fab0354faa82b765e34d791cae3d6775e5668ccb
Christian Morbidoni <christian.morbidoni@gmail.com>
2018-09-13 09:37:53 +0200

    new expes

--- Text_Classifier/experiments.py
+++ Text_Classifier/experiments.py
@@ -12,19 +12,31 @@ fixed = "python main.py --cuda True --load_model False --save_model True --use_g
 #experiments_to_run.append(fixed + " --nn_model CNN --field full-text --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.5 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 10 --th 0.0 0.5 0.9")
 
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.01 --max_length 200 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
+#RUN POTENA SERVER
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 200 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
+#DONE
+#experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.01 --max_length 200 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.01 --max_length 500 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
+#DONE
+#experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 200 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 500 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
+#DONE
+#experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.01 --max_length 500 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.01 --max_length 1000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
+#DONE
+#experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 500 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
+
+#DONE
+#experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.01 --max_length 1000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
+
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.0001 --max_length 200 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 
 experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.01 --max_length 2000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 10 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.0001 --max_length 1000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
+
+#CANCELED
+#experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.01 --max_length 2000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 10 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 
 experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 2000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 10 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 

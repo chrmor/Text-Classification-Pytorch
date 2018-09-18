0000f278edbf1c38d98b2d29a156915ac29d825a
Christian Morbidoni <christian.morbidoni@gmail.com>
2018-09-18 11:37:40 +0200

    exos

--- Text_Classifier/experiments.py
+++ Text_Classifier/experiments.py
@@ -41,14 +41,17 @@ fixed = "python main.py --cuda True --load_model False --save_model True --use_g
 #DONE
 #experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 2000 --embeddings_dim 100 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 10 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 5 --th 0.0")
 
-#TODO
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 10 --th 0.0")
+#DONE - INTERRUPTED
+#experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 10 --th 0.0")
 
 #TODO
 experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 10 --th 0.0")
 
 #TODO
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1500 --embeddings_dim 300 --dropout_p 0.5 --num_sm_hidden 100 --batch_size 10 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 10 --th 0.0")
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1500 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 10 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 10 --th 0.0")
+
+#TODO
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.0001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 10 --th 0.0")
 
 for exp in experiments_to_run:
     print("Running experiment with args:\n" + exp)

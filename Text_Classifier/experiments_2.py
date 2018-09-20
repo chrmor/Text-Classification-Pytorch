98a0692446987e8e0d761d58f2206d78698bd83e
Christian Morbidoni <christian.morbidoni@gmail.com>
2018-09-20 12:36:31 +0200

    exps

--- Text_Classifier/experiments_2.py
+++ Text_Classifier/experiments_2.py
@@ -25,6 +25,16 @@ experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_
 
 experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 15 --cuda_device 1")
 
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 3 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 16 --cuda_device 1")
+
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 17 --cuda_device 1")
+
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 18 --cuda_device 1")
+
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 19 --cuda_device 1")
+
+experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 20 --cuda_device 1")
+
 
 for exp in experiments_to_run:
     print("Running experiment with args:\n" + exp)

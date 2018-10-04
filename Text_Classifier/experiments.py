cb25299338ed6f52b2ab018ec44be57f61e1130b
Christian Morbidoni <christian.morbidoni@gmail.com>
2018-10-04 11:55:39 +0200

    wiki cats exp

--- Text_Classifier/experiments.py
+++ Text_Classifier/experiments.py
@@ -1,7 +1,7 @@
 import subprocess
 
 experiments_to_run = []
-fixed = "python main.py --cuda True --load_model False --save_model True --use_gputil False --field full-text"
+fixed = "python main.py --cuda True --load_model False --save_model True --use_gputil False"
 
 #experiments_to_run.append(fixed + " --nn_model CNN --field wiki-cats --learning_rate 0.001 --max_length 200 --embeddings_dim 300 --dropout_p 0.5 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --fold 1 --early_stop True --patience 10 --th 0.0 0.5 0.9")
 
@@ -53,21 +53,11 @@ fixed = "python main.py --cuda True --load_model False --save_model True --use_g
 #DONE - INTERRUPTED
 #experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 10 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 2")
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 3 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 3")
+experiments_to_run.append(fixed + "--field wiki-cats --nn_model RCNN --learning_rate 0.001 --max_length 200 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 30 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 10 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 1")
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 4")
+experiments_to_run.append(fixed + "--field wiki-cats --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 10 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 1")
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 5")
 
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 6")
-
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 7")
-
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 8")
-
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 9")
-
-experiments_to_run.append(fixed + " --nn_model RCNN --learning_rate 0.001 --max_length 1000 --embeddings_dim 300 --dropout_p 0.1 --num_sm_hidden 100 --batch_size 15 --num_epochs 100 --y_start 2010 --y_end 2018 --dataset 30-fold-8-classes --early_stop True --patience 2 --th 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 --fold 10")
 
 for exp in experiments_to_run:
     print("Running experiment with args:\n" + exp)
